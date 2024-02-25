267451dfbff5cce541941200b3bae5e80d0a164c
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-02-25 10:45:56 +0300

    finally completed by Ali

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// Declare customerName in global scope and initialize it to 'bob'
+var customerName = "bob";
+const leastFavoriteCustomer = "some value";
+
+// Function that returns the customerName
+function getCustomerName() {
+    return customerName;
+}
+
+// Function that modifies the customerName variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Function that sets the bestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Assuming bestCustomer should be declared globally
+}
+
+// Function that overwrites the best customer
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"; // Modifying the global variable
+}
+
+// Function that unsuccessfully tries to reassign the least favorite customer
+function changeLeastFavoriteCustomer() {
+    // This will result in an error because leastFavoriteCustomer is a constant
+    leastFavoriteCustomer = "hi";
+}
+
+ 
